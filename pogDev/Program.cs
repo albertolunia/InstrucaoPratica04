299c7521c11b5a7539c95a880a76c49c71ab596a
AllanaCampos <69643805+AllanaCampos@users.noreply.github.com>
2023-12-06 11:16:22 -0300

    Delete pogDev directory

--- pogDev/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿﻿using pogDev;
-
-List<Paciente> pacientes = new List<Paciente>();
-List<Medico> medicos = new List<Medico>();
-List<Exame> exames = new List<Exame>();
-List<Atendimento> atendimentos = new List<Atendimento>();
-
-Consultorio.menu(pacientes,medicos,exames,atendimentos);
